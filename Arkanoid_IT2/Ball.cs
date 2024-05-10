4f8bbc64dd0955e59ed7a79b115c752236dca780
Danek <Danek@DANIELROUPA>
2024-05-10 20:32:50 +0200

    Přidejte soubory projektu.

---
+++ Arkanoid_IT2/Ball.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+
+namespace Arkanoid_IT2
+{
+    public class Ball : GameObject
+    {
+
+        public override void Draw(Canvas canvas)
+        {
+            Ellipse Ellipse = new Ellipse();
+            Ellipse.Width = 15;
+            Ellipse.Height = 15;
+            Ellipse.Fill = Brushes.Red;
+            canvas.Children.Add(Ellipse);
+        }
+    }
+}
